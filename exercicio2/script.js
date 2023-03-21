aaa6eecc3d20b1ba5a00a798d674803daa22e6c0
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-03-21 16:24:17 -0300

    resolução dos exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log(`O tamanho da string com os espaços é: ${minhaString.length}`)
+
+const minhaNovaString = minhaString.trim()
+console.log(minhaNovaString)
+console.log(`O tamanho da string sem os espaços é: ${minhaNovaString.length}`)
+
+//console.log(minhaNovaString.replace("________", "sticioso"))
+const minhaStringAtualizada = minhaNovaString.replace("________", "sticioso")
+console.log(minhaStringAtualizada)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,26 @@
 //Crie a const para a frase aqui
+//A)
+
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+console.log(frase)
+
+//B)
+
+const fraseTroca = frase.replace("verde","rosa").replace("azul","laranja")
+console.log(fraseTroca)
+
+//C)
+
+console.log(fraseTroca.includes("verde","laranja"))
+
+//Extra 
+
+const fraseCasa = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, '
+
+const fraseGato = 'mas não deixe o gato sair"'
+
+const novaCasa = fraseCasa + fraseGato.toUpperCase()
+console.log (novaCasa.replace("verde","rosa").replace("azul","laranja"))
+
+
